e406000072712b2fbb1f27bc9f9e3cf1ce9edd13
HootDunk <58009556+HootDunk@users.noreply.github.com>
2021-04-06 21:21:01 -0500

    added classes to represent and drive the game forward, with related tests

---
+++ src/game-logic/Gameboard.js
@@ -0,0 +1,49 @@
+// we will have two gameboards one for player and one for AI
+class Gameboard {
+  constructor() {
+    this.grid = Array.from(Array(10), () => new Array(10));
+    // keep array of ship references to track how many have been sank
+    this.ships = [];
+    this.allShipsSank = false;
+  }
+  // add reference to ship object at each given coordinate
+  placeShip(ship, coordinates){
+    coordinates.forEach(obj => {
+      this.grid[obj.vertical][obj.horizontal] = ship;
+    })
+    ship.setLocation(coordinates)
+  }
+
+  receiveAttack(vertical, horizontal){
+    // see if attack hit a ship
+    if (this.grid[vertical][horizontal] === undefined){
+      this.grid[vertical][horizontal] = "miss";
+    }
+    // is not undefined, is not miss. therefore it is a ship
+    // call hit function on that ship
+    else if (typeof this.grid[vertical][horizontal] === 'object'){
+      let shipObj = this.grid[vertical][horizontal];
+      shipObj.hit(vertical, horizontal);
+    }
+    else console.log("move not allowed")
+  }
+
+  areAllSank(){
+    for (const ship of this.ships){
+      if (!ship.sank){
+        // a ship is still standing
+        return false;
+      }
+    }
+    // all ships have sunk
+    return true;
+  }
+
+  addShip(ship){
+    this.ships.push(ship);
+  }
+
+}
+
+
+export { Gameboard }
---
+++ src/game-logic/Ship.js
@@ -0,0 +1,53 @@
+
+class Ship {
+  constructor(length, name){
+    // length to represent ship prior to placement on board
+    this.length = length;
+    // array of objects recording ship location and hit status
+    this.location = new Array(length).fill(null);
+    /*
+    locationObj = {
+      vertical: y,
+      horizontal: x,
+      isHit: true/false
+    }
+    */
+    this.sank = false;
+    this.name = name;
+  }
+
+  // set isHit = true for the matching location object
+  hit(vertical, horizontal) {
+    for (const obj of this.location){
+      if (obj.vertical === vertical && obj.horizontal === horizontal){
+        obj.isHit = true;
+        break;
+      }
+    }
+    if (this.isSunk()){
+      this.sank = true;
+    }
+  }
+
+  // pass in array of coordinate objects to set ship location on gameboard
+  setLocation(coordinates) {
+    // update each coordinate object with isHit = false;
+    let newLocation = coordinates.map(obj => {
+      return obj = {...obj, isHit: false}
+    })
+    this.location = newLocation;
+  }
+
+  isSunk() {
+    for (const obj of this.location){
+      if (obj.isHit === false){
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
+
+
+export { Ship }
